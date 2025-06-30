bfc9651094bc972c17429ea818157b28cfd473bf
Noviiich <nowikoff63@gmail.com>
2025-06-30 21:47:31 +0300

    fix: добавлена теги для модели пользователя

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -3,8 +3,8 @@ package domain
 import "time"
 
 type User struct {
-	ID          int       `json:"id"`
-	Name        string    `json:"name"`
-	Age         int       `json:"age"`
-	CreatedDate time.Time `json:"created_date"`
+	ID          int       `json:"id" db:"id"`
+	Name        string    `json:"name" db:"name"`
+	Age         int       `json:"age" db:"age"`
+	CreatedDate time.Time `json:"created_date" db:"created_date"`
 }

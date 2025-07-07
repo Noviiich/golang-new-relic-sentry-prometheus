db511c8ce94a2ed9550d53072860768cc1181638
Alex Novikov <150938218+Noviiich@users.noreply.github.com>
2025-07-07 09:44:37 +0000

    refactor: Перен модель пользователя

---
+++ domain/user.go
@@ -0,0 +1,12 @@
+package domain
+
+import (
+	"time"
+)
+
+type User struct {
+	ID          uint      `gorm:"primaryKey" json:"id"`
+	Name        string    `json:"name"`
+	Age         int       `json:"age"`
+	CreatedDate time.Time `json:"created_date"`
+}
